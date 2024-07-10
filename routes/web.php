9d13f84dce5b5952ac94942207481e676d0f5b6e
CaweAlves <contato@cawe.dev>
2024-07-10 09:30:55 -0300

    creating chat routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,43 @@
 <?php
 
+use App\Events\MessageSent;
+use App\Models\ChatMessage;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/chat/{friend}', function (User $friend) {
+    return view('chat', [
+        'friend' => $friend
+    ]);
+})->middleware(['auth'])->name('chat');
+
+Route::get('/messages/{friend}', function (User $friend) {
+    return ChatMessage::query()
+        ->where(function ($query) use ($friend) {
+            $query->where('sender_id', auth()->id())
+                ->where('receiver_id', $friend->id);
+        })
+        ->orWhere(function ($query) use ($friend) {
+            $query->where('sender_id', $friend->id)
+                ->where('receiver_id', auth()->id());
+        })
+        ->with(['sender', 'receiver'])
+        ->orderBy('id', 'asc')
+        ->get();
+})->middleware(['auth']);
+
+Route::post('/messages/{friend}', function (User $friend) {
+    $message = ChatMessage::create([
+        'sender_id' => auth()->id(),
+        'receiver_id' => $friend->id,
+        'text' => request()->input('message')
+    ]);
+
+    broadcast(new MessageSent($message));
+
+    return $message;
 });
